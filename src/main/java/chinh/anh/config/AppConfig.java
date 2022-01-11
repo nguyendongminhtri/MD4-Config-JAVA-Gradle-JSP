b0c515fbcd881e10a804a8cc7fb1c47339eae0bb
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2022-01-11 09:49:55 +0700

    JAVA JSP GRADLE

---
+++ src/main/java/chinh/anh/config/AppConfig.java
@@ -0,0 +1,26 @@
+package chinh.anh.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        System.out.println("1");
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        System.out.println("2");
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        System.out.println("3");
+        return new String[]{"/"};
+    }
+}
+
+
